4c1c60ef79e4f775330fc091c532abdef3405d28
Syrym <justsyrym@gmail.com>
2023-04-07 13:33:05 +0200

    complete week 04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,53 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+  @i
+  M=0         // set i = 0
+  @8192
+  D=A
+  @n
+  M=D       // set n = 480
+
+  @SCREEN
+  D=A
+  @addr
+  M=D
+
+  @KBD
+  D=M
+  @key
+  M=D
+
+  (FILL_LOOP)
+  @i
+  D=M
+  @n
+  D=D-M
+  @LOOP
+  D;JEQ    // if (i == n) goto LOOP
+
+  @key
+  D=M
+  @CLEAR
+  D;JEQ    // if (key == 0) goto CLEAR
+  
+  @addr
+  A=M
+  M=-1      // set RAM[addr] = 1111111111111111
+  @NEXT
+  0;JMP
+
+  (CLEAR)
+    @addr
+    A=M
+    M=0     // set RAM[addr] = 0000000000000000
+
+  (NEXT)
+  @i
+  M=M+1   // i++
+  @addr
+  M=M+1   // addr++
+
+  @FILL_LOOP
+  0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,32 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+@i
+M=0  // i = 0
+@R2
+M=0
+
+(LOOP)
+  @i
+  D=M
+  @R1
+  D=D-M
+  @END
+  D;JEQ  // if (i == R1) goto END
+
+  @R2
+  D=M
+  @R0
+  D=D+M  // D = R2 + R0
+  @R2
+  M=D    // R2 = D
+
+  @i
+  M=M+1  // i++
+
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
